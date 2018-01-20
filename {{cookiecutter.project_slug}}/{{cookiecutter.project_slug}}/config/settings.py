6e629350cdbbf5196264ecc0f03a0b5900a7911e
sonic182 <johander1822@gmail.com>
2018-01-20 18:28:51 +0100

    Added pytest-cov and removed settings.py file

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/settings.py
+++
@@ -1,9 +0,0 @@
-"""Settings module."""
-from os.path import dirname
-from os.path import join
-from os.path import abspath
-
-
-def basepath(*args):
-    """Get files from basepath."""
-    return join(dirname(dirname(dirname(abspath(__file__)))), *args)
